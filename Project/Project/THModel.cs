f825fcba13470f727dd615b1d39aac293814c737
Wasupon <wasupont@windowslive.com>
2015-11-23 16:13:23 +0700

    test

---
+++ Project/Project/THModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project
+{
+    class THModel : Model
+    {
+        protected int boardSize; // default is 4
+        protected int[,] board;
+        protected Random rand;
+
+        public THModel () : this(4)
+        {
+            // default board size is 4 
+        }
+
+        public int[,] GetBoard()
+        {
+            return board;
+        }
+
+        public THModel(int size)
+        {
+            boardSize = size;
+            board = new int[boardSize, boardSize];
+            var range = Enumerable.Range(0, boardSize);
+            foreach (int i in range)
+            {
+                foreach (int j in range)
+                {
+                    board[i, j] = 0;
+                }
+            }
+            NotifyAll();
+        }
+    }
+}
